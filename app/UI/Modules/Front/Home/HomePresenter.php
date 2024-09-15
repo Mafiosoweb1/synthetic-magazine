c89b3345111fba99dd1630e9747dffdea60ad212
Petr Steinbauer <peta.steinbauer@gmail.com>
2024-09-15 14:05:07 +0200

    show newest article in front homepage

--- app/UI/Modules/Front/Home/HomePresenter.php
+++ app/UI/Modules/Front/Home/HomePresenter.php
@@ -1,10 +1,20 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\UI\Modules\Front\Home;
 
 use App\UI\Modules\Front\BaseFrontPresenter;
+use Doctrine\ORM\Exception\NotSupported;
 
 final class HomePresenter extends BaseFrontPresenter
 {
+	/**
+	 * @throws NotSupported
+	 */
+	public function actionDefault(): void
+	{
+		//if header is not empty
+		$articles = $this->entityManager->getArticleRepository()->findBy([], ['updatedAt' => 'DESC'], 10);
+		$this->getTemplate()->articles = $articles;
+	}
 
 }
