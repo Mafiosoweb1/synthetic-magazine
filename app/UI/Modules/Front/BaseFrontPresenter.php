90e5f8bcb202bdbfe93cb47398f5261c669daa11
Petr Steinbauer <peta.steinbauer@gmail.com>
2024-09-15 14:05:07 +0200

    show newest article in front homepage

--- app/UI/Modules/Front/BaseFrontPresenter.php
+++ app/UI/Modules/Front/BaseFrontPresenter.php
@@ -2,9 +2,18 @@
 
 namespace App\UI\Modules\Front;
 
+use App\Model\Database\EntityManagerDecorator;
 use App\UI\Modules\Base\UnsecuredPresenter;
+use Doctrine\ORM\EntityManager;
 
 abstract class BaseFrontPresenter extends UnsecuredPresenter
 {
 
+	protected EntityManagerDecorator $entityManager;
+
+	public function __construct(EntityManagerDecorator $entityManager)
+	{
+		parent::__construct();
+		$this->entityManager = $entityManager;
+	}
 }
