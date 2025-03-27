c52da0765e3d706be5d177c6f04baa1bf53a77e9
Petr Steinbauer <peta.steinbauer@gmail.com>
2025-03-27 22:17:22 +0100

    Typo

--- app/Console/ArticleCreateCommand.php
+++ app/Console/ArticleCreateCommand.php
@@ -43,90 +43,85 @@ class ArticleCreateCommand extends Command
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-
 		$count = $input->getArgument('count');
-		for ($i = 1; $i <= $count; $i++) {
+		for ($i = 1; $i <= $count; $i++) { //cyklus pro vytvoření článků
+			$wikiList = $this->getWikiData(); //získání dat z wiki
+			foreach ($wikiList['query']['pages'] as $page) { // Iterace přes články
 
-			//nacteme clanky z wiki API
-			$url = new Url('https://cs.wikipedia.org/w/api.php');
-			$url->setQueryParameter('action', 'query');
-			$url->setQueryParameter('generator', 'random');
-			$url->setQueryParameter('grnnamespace', '0');
-			$url->setQueryParameter('grnlimit', '1');
-			$url->setQueryParameter('prop', 'extracts|pageimages');
-			$url->setQueryParameter('exintro', '');
-			$url->setQueryParameter('explaintext', '');
-			$url->setQueryParameter('piprop', 'original');
-			$url->setQueryParameter('format', 'json');
+				// test 1 Má článek z wiki méně než 100 znaků - pokračuj na další iteraci
+				if (strlen($page['extract']) < 100) { continue; }
+				// test 2 obsahuje v názvu slovo Rozcestník - pokračuj na další iteraci
+				if (str_contains($page['title'], 'Rozcestník')) { continue; }
 
-			$wikiData = FileSystem::read($url->getAbsoluteUrl());
-			$wikiList = json_decode($wikiData, true);
-			//dump($wikiList);
+				$pictureUrl = $this->findImage($page); //nalezení obrázku
 
-
-			// Iterace přes články
-			foreach ($wikiList['query']['pages'] as $page) {
-				$foundValidImage = false; // Flag pro určení, zda byl nalezen platný obrázek
-				$pictureUrl = null;
-
-
-				//dumpe($page);
-				if (isset($page['original']) && isset($page['original']['source'])) {
-					//koncovka musí být obrázek
-					$exeptions = [
-						'.svg',
-						'.gif',
-						'.jpeg',
-						'.webp',
-						'.png',
-						'.jpg',
-					];
-					$foundValidImage = false;
-					foreach ($exeptions as $exeption) {
-						if (str_ends_with($page['original']['source'], $exeption)) {
-							$foundValidImage = true;
-							break;
-						}
-					}
-					if ($foundValidImage) {
-						$pictureUrl = $page['original']['source'];
-					}
-				}
-
-				//1/ is short
-				if (strlen($page['extract']) < 100) {
-					continue;
-				}
-				//2/ contains "Rozcestník" in title or extract - use str_contains
-				if (str_contains($page['title'], 'Rozcestník') || str_contains($page['extract'], 'Rozcestník')) {
-					continue;
-				}
-
-				$article = new Article();
+				$article = new Article(); //vytvoření nového článku
 				$article->setCreatedAt();
 				$article->setHeading('');
 				$article->setContent('');
 				$article->setWikiId($page['pageid']);
-				$article->setPicture($pictureUrl);
+				$article->setPicture($pictureUrl); //nastavení obrázku
 				$article->setSourceHeading($page['title']);
 				$article->setSourceContent($page['extract']);
 				$article->setStatus(Article::STATUS_CONCEPT);
-				$this->entityManager->persist($article);
-
-				//dump($page);
-
+				$this->entityManager->persist($article); //uložení článku
 
 				try {
-					$this->entityManager->flush();
-				} catch (Exception $e) {
+					$this->entityManager->flush(); //uložení do databáze
+				} catch (Exception $e) { //chyba při ukládání
 					$output->writeln('Error: ' . $e->getMessage());
 					continue;
 				}
-			}
-
+			} //konec foreach,když je continue tak se přeskočí na další iteraci
 			$output->writeln('Article created.');
 		}
+		$output->writeln('All articles created.');
 		return 0;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getWikiData(): mixed
+	{
+		//nacteme clanky z wiki API
+		$url = new Url('https://cs.wikipedia.org/w/api.php'); //sestavení url
+		$url->setQueryParameter('action', 'query');
+		$url->setQueryParameter('generator', 'random');
+		$url->setQueryParameter('grnnamespace', '0');
+		$url->setQueryParameter('grnlimit', '1');
+		$url->setQueryParameter('prop', 'extracts|pageimages');
+		$url->setQueryParameter('exintro', '');
+		$url->setQueryParameter('explaintext', '');
+		$url->setQueryParameter('piprop', 'original');
+		$url->setQueryParameter('format', 'json');
+
+		//načtení dat z url(absolutní url)
+		$wikiData = FileSystem::read($url->getAbsoluteUrl());
+		return json_decode($wikiData, true); //dekódování jsonu
+	}
+
+	/**
+	 * @param mixed $page
+	 * @return string|null
+	 */
+	public function findImage(mixed $page): ?string
+	{
+		$exceptions = ['.svg','.gif','.jpeg','.webp','.png','.jpg']; //koncovka musí být obrázek
+		$pictureUrl = null;  //url obrázku, defaultně null
+		if (isset($page['original']) && isset($page['original']['source'])) { //pokud existuje obrázek
+			$foundValidImage = false; //obrázku nevěříme
+			foreach ($exceptions as $exception) {
+				if (str_ends_with($page['original']['source'], $exception)) {
+					$foundValidImage = true; //obrázek je validní
+					break; //ukončení foreach
+				}
+			} //konec break
+			if ($foundValidImage) { //pokud je obrázek validní
+				$pictureUrl = $page['original']['source']; //nastavení obrázku
+			}
+		}
+		return $pictureUrl;
 	}
 
 }
